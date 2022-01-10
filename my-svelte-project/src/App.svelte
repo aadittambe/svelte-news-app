bb5e90c98aea55039e0db74427e26f84cc1d6b1b
Aadit Tambe <aadit.tambe@gmail.com>
2022-01-10 18:21:07 -0500

    setup is ready

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let name;
+  import schools from "./schools.json";
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  {#each schools as school}
+    {school}
+  {/each}
+  <!-- continue from https://www.youtube.com/watch?v=fm2r6JY5h9E&t=396s -->
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
