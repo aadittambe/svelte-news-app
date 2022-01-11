140b7719cd8bdd5d04034156a1aa9aca54c9c08c
Aadit Tambe <aadit.tambe@gmail.com>
2022-01-10 23:16:35 -0500

    x

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,23 +1,40 @@
 <script>
-  export let name;
   import schools from "./schools.json";
+  let selectedSchool;
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
-  {#each schools as school}
-    {school}
-  {/each}
+  {#if selectedSchool}
+    <p>
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a on:click={() => (selectedSchool = null)} href="#">
+        Go back to school list
+      </a>
+    </p>
+    <h1>School is {selectedSchool.SCHOOLNAME}</h1>
+    <p>The education level is {selectedSchool.SCH_TYPE}</p>
+    <p>The principal is {selectedSchool.PRINCIPAL}</p>
+  {:else}
+    <ul>
+      <h3>NYC school list</h3>
+      <p>Select a school to read some information about it</p>
+      {#each schools as school}
+        <li>
+          <!-- svelte-ignore a11y-invalid-attribute -->
+          <a on:click={() => (selectedSchool = school)} href="#">
+            {school.SCHOOLNAME}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+
   <!-- continue from https://www.youtube.com/watch?v=fm2r6JY5h9E&t=396s -->
 </main>
 
 <style>
   main {
-    text-align: center;
+    text-align: left;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
